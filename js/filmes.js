a25d41ab8d81f4d77d39d0433e7aea8526d52193
LEONARDO TORQUATO DE ANDRADE <23111738@CFP127ALUNO.LOCAL>
2024-03-05 12:05:00 -0300

    Login Feito

---
+++ js/filmes.js
@@ -0,0 +1,14 @@
+export async function getFilmes (){
+    const url = 'http://10.107.134.43:8080/v2/acme/filmes'
+    const response = await fetch(url)
+    const data = await response.json()
+    return data.filmes
+
+}
+
+export async function getFilmes (id){
+    const url = `http://10.107.134.43:8080/v2/acme/filme/${id}`
+    const response = await fetch (url)
+    const data = await response.json()
+    return data.filme[0]
+}
